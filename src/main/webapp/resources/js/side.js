2891b110d35966d495dcfe39c309deff4c055adc
Sikdorak <daehoo1993@gmail.com>
2023-09-03 23:16:13 +0900

    230903 tb project

--- src/main/webapp/resources/js/side.js
+++ src/main/webapp/resources/js/side.js
@@ -1,3 +1,9 @@
-/**
- * 
- */
+function addBurger(Id,Name,Price){
+    $.ajax({
+        url:"/api/updateBurger",
+        method:"GET",
+        data:{
+            "p_id": Id,
+            "p_name": Name,
+            "p_price": Price
+        },
