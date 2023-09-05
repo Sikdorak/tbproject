cdc8a35706358b44bebeac2588748f496dff6757
Sikdorak <daehoo1993@gmail.com>
2023-09-05 16:14:00 +0900

    230905 tb project

--- src/main/webapp/resources/js/side.js
+++ src/main/webapp/resources/js/side.js
@@ -7,3 +7,17 @@ function addBurger(Id,Name,Price){
             "p_name": Name,
             "p_price": Price
         },
+        success:function(response){
+            $(".orderList").empty();
+            console.log(response)
+            for(var i = 0; i < response.length; i++){
+                var menu = response[i];
+                var menuData = menu.p_name + " " + menu.p_price + "<br>"
+                $(".orderList").append(menuData);
+            }
+        },
+        error:function(xhr, status, error){
+            console.error(error)
+        }
+    });
+}
